9b327a0805ab78311e6944a02257db10d04bad08
Laura Vilela <73198219+LauraVilela@users.noreply.github.com>
2025-02-02 20:40:03 -0300

    Update batalhaNaval.h

--- batalhaNaval.h
+++ batalhaNaval.h
@@ -1,1 +1,23 @@
+#ifndef BATALHA_NAVAL_H
+#define BATALHA_NAVAL_H
+
+#include <vector>
+#include "coordenadas.h"
+#include "navio.h"
+#include "plataforma.h"
+#include "partida.h"
+#include "dadosPartidas.h"
+#include "usuario.h"
+#include "adversario.h"
+
+class BatalhaNaval {
+private:
+    Dados dados;
+public:
+    BatalhaNaval();
+    void iniciar(); 
+    std::vector<Coordenada> gerarPosicaoAleatoria(int tamanhoNavio, int tamanhoTabuleiro, std::vector<std::vector<bool>>& tabuleiro);
+};
+
+#endif
 
