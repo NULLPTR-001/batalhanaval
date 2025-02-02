357c6c64e1d7de65383d77a9c4499e588b915529
Laura Vilela <73198219+LauraVilela@users.noreply.github.com>
2025-02-02 20:41:22 -0300

    Update partida.h

--- partida.h
+++ partida.h
@@ -1,1 +1,21 @@
+#ifndef PARTIDA_A
+#define PARTIDA_A
 
+#include "jogadores.h"
+#include "plataforma.h"
+#include "adversario.h"
+#include <string>
+
+class Partida {
+private:
+    Plataforma& plataforma;  
+    Jogadores* player1;
+    Jogadores* player2;
+    std::string vencedor;
+public:
+    Partida(Plataforma& plataforma, Jogadores* player1, Jogadores* player2);
+    void iniciar();
+    std::string getVencedor() const;
+};
+
+#endif
