114369d0573ed390301e65c81981d7ecdb215ce3
Laura Vilela <73198219+LauraVilela@users.noreply.github.com>
2025-02-02 20:45:18 -0300

    Update partida.cpp

--- partida.cpp
+++ partida.cpp
@@ -1,1 +1,96 @@
+#include "partida.h"
+#include <iostream>
+
+Partida::Partida(Plataforma& plataforma, Jogadores* player1, Jogadores* player2)
+    : plataforma(plataforma), player1(player1), player2(player2), vencedor("") {
+}
+
+void Partida::iniciar() {
+    int acertosJogador1 = 0, acertosJogador2 = 0;
+    int rodadas = 0;
+
+    std::cout << "\n";
+    std::cout << "Tabuleiro inicial:" << std::endl;
+    plataforma.exibeTabuleiro();
+
+    while (rodadas < 5 || (player1->getVidas() > 0 && player2->getVidas() > 0)) {
+        rodadas++;
+
+        std::cout << "\n";
+        std::cout << "Vez de " << player1->getNome() << " (" << player1->getVidas() << " vidas restantes):" << std::endl;
+        Disparo disparoJogador1 = player1->disparar();
+        bool acertoJogador1 = plataforma.registroDisparo(disparoJogador1);
+
+        if (acertoJogador1) {
+            std::cout << player1->getNome() << " acertou um navio!" << std::endl;
+            acertosJogador1++;
+        }
+        else {
+            std::cout << player1->getNome() << " acertou a água!" << std::endl;
+            player1->perderVida();
+        }
+
+        plataforma.exibeTabuleiro();
+
+        if (player1->getVidas() == 0) {
+            std::cout << player1->getNome() << " perdeu todas as vidas." << std::endl;
+        }
+
+        if (rodadas == 5 && acertosJogador1 == 0 && acertosJogador2 == 0) {
+            if (player1->getVidas() == 1 && player2->getVidas() >= 2) {
+                break;
+            }
+        }
+
+        std::cout << "\n";
+        std::cout << "Vez de " << player2->getNome() << " (" << player2->getVidas() << " vidas restantes):" << std::endl;
+        Disparo disparoJogador2 = player2->disparar();
+        bool acertoJogador2 = plataforma.registroDisparo(disparoJogador2);
+
+        if (acertoJogador2) {
+            std::cout << player2->getNome() << " acertou um navio!" << std::endl;
+            acertosJogador2++;
+        }
+        else {
+            std::cout << player2->getNome() << " acertou a água!" << std::endl;
+            player2->perderVida();
+        }
+
+        plataforma.exibeTabuleiro();
+
+        if (player2->getVidas() == 0) {
+            std::cout << "\n";
+            std::cout << player2->getNome() << " perdeu todas as vidas." << std::endl;
+        }
+
+        if (rodadas == 5 && acertosJogador1 == 0 && acertosJogador2 == 0) {
+            vencedor = "Empate";
+            break;
+        }
+
+        if (rodadas == 5 && (acertosJogador1 > 0 || acertosJogador2 > 0)) {
+            vencedor = (acertosJogador1 > acertosJogador2) ? player1->getNome() : player2->getNome();
+            break;
+        }
+
+        if (rodadas >= 5 && player1->getVidas() == 0 && player2->getVidas() > 1) {
+            vencedor = player2->getNome();
+            break;
+        }
+        if (rodadas >= 5 && player2->getVidas() == 0 && player1->getVidas() > 1) {
+            vencedor = player1->getNome();
+            break;
+        }
+    }
+
+    std::cout << player1->getNome() << " acertou " << acertosJogador1 << " partes do navio." << std::endl;
+    std::cout << player2->getNome() << " acertou " << acertosJogador2 << " partes do navio." << std::endl;
+    std::cout << "\n";
+    std::cout << "Fim de jogo! Vencedor: " << vencedor << std::endl;
+    std::cout << "\n";
+}
+
+std::string Partida::getVencedor() const {
+    return vencedor;
+}
 
