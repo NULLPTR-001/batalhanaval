8716bc0e4eeca7aef9e23239852d7fed8c3ae3f2
Thalesj18 <thakes22@gmail.com>
2025-02-03 02:35:51 -0300

    Update dadosPartidas.cpp

--- dadosPartidas.cpp
+++ dadosPartidas.cpp
@@ -1,4 +1,5 @@
 #include "dadosPartidas.h"
+#include <ctime>
 #include <iostream>
 #include <fstream>
 
@@ -6,6 +7,13 @@ Dados::Dados(std::string arquivo) : arquivo(arquivo) {
 }
 
 void Dados::salvarPartida(std::string vencedor) {
+    // Criar diretório "data" caso não exista
+    #ifdef _WIN32
+        system("if not exist data mkdir data");
+    #else
+        system("mkdir -p data");
+    #endif
+
     std::ofstream file(arquivo, std::ios::app);
     if (file.is_open()) {
         file << vencedor << std::endl;
@@ -14,6 +22,8 @@ void Dados::salvarPartida(std::string vencedor) {
 }
 
 void Dados::mostrarDados() const {
+    // Verifica se o arquivo existe antes de tentar abrir
+    std::ofstream temp(arquivo, std::ios::app); temp.close();
     std::ifstream file(arquivo);
     if (!file.is_open()) {
         std::cout << "Nenhum historico de partidas encontrado.\n";
